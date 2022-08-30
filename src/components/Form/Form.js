ec8012c11c6ee4485631a931efa239a6bd528dfb
drili <DriliB@hotmail.com>
2022-08-30 22:11:44 +0200

    :hammer: Create A Post

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import useStyles from "./styles"
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase from "react-file-base64"
+import { useDispatch } from "react-redux"
+import { createPost } from '../../actions/posts'
 
 const Form = () => {
     const [postData, setPostData] = useState({
@@ -11,15 +13,20 @@ const Form = () => {
         tag: "",
         selectedFile: ""
     })
+
     const classes = useStyles()
     
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
 
+        dispatch(createPost(postData))
     }
 
     const clear = () => {
 
     }
+
+    const dispatch = useDispatch()
 
     return (
         <Paper className={classes.paper}>

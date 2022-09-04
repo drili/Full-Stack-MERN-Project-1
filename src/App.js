d24bdae1ebddbe9d7bc32ec277e0f163d49cb9dc
drili <DriliB@hotmail.com>
2022-09-04 18:39:57 +0200

    :lock: AUTH | Layout

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core"
-import memories from "./images/memories.png"
-import Posts from "./components/Posts/Posts"
-import Form from "./components/Form/Form"
-import useStyles from "./styles"
-import { useDispatch } from 'react-redux'
-import { getPosts } from "./actions/posts"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+import Auth from './components/Auth/Auth'
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null)
-    const classes = useStyles()
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(getPosts())
-    }, [currentId, dispatch])
-
     return (
-        <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
-                <img className={classes.image} src={memories} height="60"></img>
-            </AppBar>
-
-            <Grow in>
-                <Container>
-                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}></Posts>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>  
-                            <Form currentId={currentId} setCurrentId={setCurrentId}></Form>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxwidth="lg">
+                <Navbar></Navbar>
+                <Routes>
+                    <Route path="/" exact element={<Home />}></Route>
+                    <Route path="/auth" exact element={<Auth />}></Route>
+                </Routes>
+            </Container>
+        </BrowserRouter>
     )
 }
 
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Auth() {
+    const state = null
+
+    return (
+        <div>Auth</div>
+    )
+}
+
+export default Auth
